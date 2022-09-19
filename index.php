3d8d4618841e353ca408358483463001e9dad340
Ashishbindapi <112742689+Ashishbindapi@users.noreply.github.com>
2022-09-19 11:58:05 +0530

    commit

--- index.php
+++ index.php
@@ -1,16 +1,43 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.min.css">
     <title>Document</title>
 </head>
 <body>
-    <form  method="post">
-        <input type="text" name="first_name" placeholder="First Name">
-        <input type="text" name="second_name" placeholder="Second Name">
-        <input type="text" name="mobile" placeholder="mobile Number">
+    <div class="col-12 text-center">
+         <?php
+              $servername='localhost';
+              $username='root';
+              $password='';
+              $dbname = "dev7";
+              
+              $conn=mysqli_connect($servername,$username,$password,$dbname);
+              
+              if(isset($_POST['submit']))
+              {
+                  $first_name = $_POST['first_name'];
+                  $second_name = $_POST['second_name'];
+                  $mobile = $_POST['mobile'];
+                  
+                  $sql = "INSERT INTO form (first_name,second_name,mobile)
+                          VALUES ('$first_name','$second_name','$mobile')";
+                  
+                          if(mysqli_query($conn, $sql))
+                          {
+                              echo "<h4>success</h4>";
+                          } 
+              }
+            ?>
+    </div>
+    <form  method="post" class="text-center mt-4">
+        <input type="text" name="first_name" placeholder="First Name" class="mt-2"><br>
+        <input type="text" name="second_name" placeholder="Second Name"class="mt-2"><br>
+        <input type="text" name="mobile" placeholder="mobile Number"class="mt-2"><br>
         <button type="submit"name="submit">Submit</button>
         <?php
            /* if(isset($_POST['submit']))
@@ -24,25 +51,3 @@
     </form>
 </body>
 </html>
-<?php
- $servername='localhost';
- $username='root';
- $password='';
- $dbname = "dev7";
- $conn=mysqli_connect($servername,$username,$password,$dbname);
-
- if(isset($_POST['submit']))
- {
-    $first_name = $_POST['first_name'];
-    $second_name = $_POST['second_name'];
-    $mobile = $_POST['mobile'];
-    
-     $sql = "INSERT INTO form (first_name,second_name,mobile)
-	         VALUES ('$first_name','$second_name','$mobile')";
-    
-            if(mysqli_query($conn, $sql))
-            {
-                echo "success";
-            } 
- }
-?>
